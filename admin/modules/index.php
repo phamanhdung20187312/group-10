f335aac3fe648dd74bb52aedf4496cda57af354c
ntt2910 <62978231+ntt2910@users.noreply.github.com>
2022-06-15 21:06:42 +0700

    Add files via upload

---
+++ admin/modules/index.php
@@ -0,0 +1,31 @@
+<?php 
+    require_once ("check_login.php");
+    require_once ("../layouts/header.php");
+?>
+    <!--content!------>
+    <div id="content-wrapper">
+        <div class="container-fluid">
+            <!-- Breadcrumbs-->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="index.php">Dashboard</a>
+                </li>
+                <li class="breadcrumb-item active">Blank Page</li>
+            </ol>
+            <!-- Page Content -->
+            <h1>Xin chào admin :")</h1>
+            <p>Vào rồi thì làm việc đi :)</p>
+
+        </div>
+        <!-- /.container-fluid -->
+        <!-- Sticky Footer -->
+        <footer class="sticky-footer">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright © Your Website 2019</span>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- /.content-wrapper -->
+<?php require_once ("../layouts/footer.php")?>
