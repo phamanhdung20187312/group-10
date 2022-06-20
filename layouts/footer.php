dd5050f435c74df24d53b39c68200b6eb2fafaa3
Dương Việt Anh <62955210+SnowFlake11111@users.noreply.github.com>
2022-06-20 11:42:51 +0700

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,13 @@
+<ter -->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">NTA & TVA</p>
+        </div>
+        <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="/nta/public/frontend/vendor/jquery/jquery.min.js"></script>
+    <script src="/nta/public/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    </body>
